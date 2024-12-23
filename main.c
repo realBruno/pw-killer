768a48eaae71893556b98649265cfdcc2474da0a
realBruno <soubruno@protonmail.com>
2024-12-23 20:49:15 -0300

    refactor: big changes

--- main.c
+++ main.c
@@ -2,17 +2,18 @@
 *   PW-KILLER
 *   Author: Bruno Fernandes (github.com/realBruno)
 *   Creation date: 21/12/2024
-*   Last updated: 21/12/2024
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 
-#include "zip/winrar.h"
-#include "zip/7zip.h"
-#include "zip/unzip.h"
+#define MAX_PATH_SIZE 51
+#define SYSTEM_ARGUMENT 201
+
+void generate_password(int x);
 
 int main(void)
 {   
@@ -20,37 +21,80 @@ int main(void)
     printf("Before using, see \"Guide\" on README.md\n\n");
     usleep(1000000);
 
-    int filetype;
-    printf("SELECT YOUR FILETYPE\n");
-    printf("Zip (1) | PDF (2): ");
-    scanf("%d", &filetype);
+    printf("Insert file path (%d char. max): ", MAX_PATH_SIZE - 1);
+    char file_path[MAX_PATH_SIZE];
+    scanf("%s", file_path);
     
-    switch(filetype)
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) // clears buffer
+        ;
+    
+    printf("Insert where to extract file (%d char. max): ", MAX_PATH_SIZE - 1);
+    char extraction_path[MAX_PATH_SIZE];
+    scanf("%s", extraction_path);
+
+    // BUILDING COMMAND TO EXTRACT FILE
+    char command[SYSTEM_ARGUMENT] = "7z x ";
+    int command_size = sizeof(command) - strlen(command) - 1;
+    strncat(command, file_path, command_size);
+    strncat(command, " -o", command_size);
+    strncat(command, extraction_path, command_size);
+    strncat(command, " -p", command_size);
+
+    // ABOUT THE PASSWORD
+    printf("\nInclude characters to the password.\nKeep in mind that "
+            "the more types you include, the less likely it is for"
+            " the program to find the correct password.\n");
+
+    char include_numbers;
+    printf("Include numbers? (y/n): ");
+    scanf("%c", &include_numbers);
+    
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+
+    char include_upper;
+    printf("Include uppercase letters? (y/n): ");
+    scanf("%c", &include_upper);
+
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+
+    char include_lower;
+    printf("Include lowercase letters? (y/n): ");
+    scanf("%c", &include_lower);
+
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+
+    char include_special;
+    printf("Include special characters? (y/n): ");
+    scanf("%c", &include_special);
+
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+
+    int up_to;
+    printf("Insert maximum length the password may reach (0-50): ");
+    scanf("%d", &up_to);
+
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+
+    strncat(command, "123", command_size);
+    while (system(command) != 0)
     {
-        case 1:
-            int file_archive;
-            printf("SELECT FILE ARCHIVE\n");
-            printf("7-zip (1) | WinRAR (2) | Unzip (3): ");
-            scanf("%d", &file_archive);
-            break;
+        ;
     }
 
-    char c;
-    while ((c = getchar()) != '\n' && c != EOF)
-        ;
-
-    printf("Insert file path (100 char. max): ");
-    char arr[101];
-    scanf("%s", arr);
-
-    while ((c = getchar()) != '\n' && c != EOF)
-        ;
-
-    FILE *input = fopen(("%s", arr), "w");
-
-    fprintf(input, "%d", 123);
-
-    fclose(input);
-
     return 0;
+}
+
+// FUNCTION DEFINITION
+void generate_password(int x) // x is the max length the password can reach
+{                            // aka "up_to"
+    int password_len = rand() % x;
+    char password[password_len];
+
+    
 }
